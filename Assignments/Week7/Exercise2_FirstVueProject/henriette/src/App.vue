708f36ec6373c3f042ae01ae2bcb590ab8588bb2
Henriette Abbednæs <Henrietteabbednaes@hotmail.com>
2022-02-21 14:26:39 +0100

    exercise2 week7 completed

---
+++ Assignments/Week7/Exercise2_FirstVueProject/henriette/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <h1> Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Results v-if="showResults" :score ="score" />
+</template>
+
+<script>
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
+export default {
+  name: 'App',
+  components: { Block, Results},
+  data(){
+return{
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
+  methods:{
+    start(){
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      this.showResults = false
+    },
+    endGame(reactionTime){
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+
+button{
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
+}
+</style>
---
+++ Assignments/Week7/Exercise2_FirstVueProject/henriette/src/components/Block.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="block" v-if="showBlock" @click="stopTimer">
+        click me
+    </div>
+  
+</template>
+
+<script>
+export default {
+    props:['delay'],
+    data(){
+        return{
+            showBlock: false, 
+            timer: null, 
+            reactionTime:0
+        }
+    },
+    mounted(){
+        setTimeout(() => {
+         this.showBlock = true 
+         this.startTimer()
+        }, this.delay)
+    }, 
+    methods:{
+        startTimer(){
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            }, 10)
+        },
+        stopTimer(){
+            clearInterval(this.timer)
+            this.$emit('end', this.reactionTime)
+        }
+    }
+}
+</script>
+
+<style>
+    .block{
+        width: 400px;
+        border-radius: 20px;
+        background: #0faf87;
+        color: white; 
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;
+    }
+</style>
