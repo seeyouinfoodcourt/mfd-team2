1afea4620b3f5d4b054b0c87b3110120b9f5eedf
Henriette Abbednæs <Henrietteabbednaes@hotmail.com>
2022-03-02 11:47:22 +0100

    testing db

---
+++ recipe-api/src/api/test-henriette/routes/test-henriette.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * test-henriette router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::test-henriette.test-henriette');
