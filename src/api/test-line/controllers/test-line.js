de5ef73b9e5e134211c395fa4699a10767929473
linesoerine <54495950+linesoerine@users.noreply.github.com>
2022-03-02 11:45:05 +0100

    test.db

---
+++ src/api/test-line/controllers/test-line.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  test-line controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::test-line.test-line');
