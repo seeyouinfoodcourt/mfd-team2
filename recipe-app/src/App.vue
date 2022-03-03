2010484bebd8f501bed211206c022bbe63372ea9
Mikkel <msjo36725@edu.ucl.dk>
2022-03-03 10:51:10 +0100

    Sass and component structure

--- recipe-app/src/App.vue
+++ recipe-app/src/App.vue
@@ -1,14 +1,22 @@
 <template>
+  <TopNav />
+
   <HelloWorld />
+
+  <BottomNav />
 </template>
 
 <script>
   import HelloWorld from './components/HelloWorld.vue';
+  import TopNav from './components/partials/TopNav.vue';
+  import BottomNav from './components/partials/BottomNav.vue';
 
   export default {
     name: 'App',
     components: {
       HelloWorld,
+      TopNav,
+      BottomNav,
     },
   };
 </script>
---
+++ recipe-app/src/components/partials/BottomNav.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>I am the bottom nav</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
