799978a07472eff78812179af5da8451ba96e5b6
henrietteabbednaes <henr8588@edu.ucl.dk>
2022-03-20 14:52:15 +0100

    Searchbar funktion(Search in recipes)

--- recipe-app/src/components/search/SearchBar.vue
+++ recipe-app/src/components/search/SearchBar.vue
@@ -8,16 +8,54 @@
     </ul>
     <div class="content">
         <i class="icofont-search-1 item"></i>
-        <input class="item" type="text" placeholder="Search recipe">
+        <input @click="isVisible = !isVisible" v-model="search" class="item" type="text" placeholder="Search recipe">
         <i class="icofont-justify-all item"></i>
     </div>
+    <div v-if="isVisible" class="options">
+        <ul>
+            <li v-for="recipe in  filteredRecipe" :key="recipe.id">
+                <p>{{recipe.attributes.Title}}</p>
+            </li>
+        </ul>
+    </div>
+  
 </div>
 
 
 </template>
 
 <script>
+import axios from 'axios'; 
+
 export default {
+name:'SearchBar',
+  data () {
+    return {
+      allRecipes: [],
+      search: '',
+      selectedItem: null,
+      isVisible:false,
+    }
+ },
+
+   computed:{
+    filteredRecipe(){
+      return this.allRecipes.filter((recipe)=>{
+        return recipe.attributes.Title.toLowerCase().match(this.search)
+      }); 
+    }    
+  },
+
+async mounted() {
+    try {
+      const response = await axios.get ('http://localhost:1337/api/recipes')
+      this.allRecipes = response.data.data; 
+      console.log(this.allRecipes)
+
+    } catch (error) {
+      this.error = error;
+    }
+  },
 
 }
 </script>
