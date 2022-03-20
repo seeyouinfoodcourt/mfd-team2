f000d1d419f22470ceeff9ac18b2e52546a0e6bc
henrietteabbednaes <henr8588@edu.ucl.dk>
2022-03-20 13:20:54 +0100

    Search bar UI

--- src/components/search/SearchBar.vue
+++ src/components/search/SearchBar.vue
@@ -1,5 +1,19 @@
 <template>
-  
+
+<div class="tabBox p-regular">
+    <ul class="tabs">
+        <li class="selected"><a href="#">Recipes</a></li>
+        <li><a href="#">Ingredients</a></li>
+        <li><a href="#">Chefs</a></li>
+    </ul>
+    <div class="content">
+        <i class="icofont-search-1 item"></i>
+        <input class="item" type="text" placeholder="Search recipe">
+        <i class="icofont-justify-all item"></i>
+    </div>
+</div>
+
+
 </template>
 
 <script>
