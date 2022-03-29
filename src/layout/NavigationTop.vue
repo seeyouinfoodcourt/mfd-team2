506e3440cd9b65345a362efd911e2d20aa6b69cf
Malene Ulm <msul36749@edu.ucl.dk>
2022-03-29 13:59:15 +0200

    NavTop new icons

--- src/layout/NavigationTop.vue
+++ src/layout/NavigationTop.vue
@@ -1,38 +1,50 @@
 <template>
-    <header>
-      <div class="header">
-        <div class="header__content">
-          <div>
-            <a href="/"><img src="img/logo/cookingtime_new.svg" alt="Recipe" class="logo-img"></a>
-          </div>
-      
-      <div class="icon">
-        <div class="plus-item"><i class='fas fa-plus plus-icon'></i></div>
-        <div class="item"><div class="notification">
-          <span><i class='far fa-bell'></i></span>
-          <span class="sign">
-            <span class="signNumber">2</span>
-          </span>
+  <header>
+    <div class="header">
+      <div class="header__content">
+        <div>
+          <a href="/"
+            ><img
+              src="img/logo/cookingtime_new.svg"
+              alt="Recipe"
+              class="logo-img"
+          /></a>
+        </div>
 
+        <div class="icon">
+          <div class="item">
+            <span class="material-icons-round">notifications</span>
+          </div>
+
+          <div class="user-container">
+            <router-link to="/login" v-if="!user"
+              ><span class="material-icons-round">person</span></router-link
+            >
+
+            <div class="login-cointer">
+              <div class="profil">
+                <router-link to="/profile" v-if="user"
+                  ><img
+                    src="/img/profil/profil2.png"
+                    alt="Profil"
+                    class="profil-img"
+                  />
+                  <div class="user-loggin">
+                    {{ user.username }}
+                  </div></router-link
+                >
+              </div>
+              <span @click="logout">
+                <router-link to="/" v-if="user" class="logout"
+                  >Logud</router-link
+                >
+              </span>
+            </div>
+          </div>
         </div>
       </div>
-
-    <div class="user-container">
-         <!-- <router-link to="/">Home</router-link> | -->
-        <!-- <router-link :to="{ name: 'Search' }">Search</router-link> | -->
-        <!-- <router-link to="/register" v-if="!user">Sign up</router-link>  -->
-        <router-link to="/login" v-if="!user"><i class="icofont-user-alt-3 user-icon"></i></router-link>
-        
-        <div class="login-cointer">
-        <div class="profil"><router-link to="/profile" v-if="user"><img src="/img/profil/profil2.png" alt="Profil" class="profil-img"><div class="user-loggin">{{ user.username }}</div></router-link></div>
-        <span @click="logout">
-          <router-link to="/" v-if="user" class="logout">Logud</router-link>
-        </span></div>
     </div>
-      </div>
-      </div>
-      </div>
-    </header>
+  </header>
 </template>
 
 <script>
@@ -42,23 +54,22 @@ export default {
     return {
       user: {},
       active: false,
-    }
+    };
   },
   mounted() {
-    this.user = JSON.parse(window.localStorage.getItem("userData"))
+    this.user = JSON.parse(window.localStorage.getItem("userData"));
   },
   methods: {
     logout() {
-      window.localStorage.removeItem("jwt")
-      window.localStorage.removeItem("userData")
-      this.$router.go('/feed')
+      window.localStorage.removeItem("jwt");
+      window.localStorage.removeItem("userData");
+      this.$router.go("/feed");
     },
-     toggle () {
-        this.active = !this.active
-      }
+    toggle() {
+      this.active = !this.active;
+    },
   },
-    
-}
+};
 </script>
 <style>
 </style>
