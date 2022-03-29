d8513d933da455ccd25f2eb2038bdb77e550f5cc
Malene Ulm <msul36749@edu.ucl.dk>
2022-03-29 13:22:40 +0200

    Added Google Material Icons

--- recipe-app/src/layout/NavigationBottom.vue
+++ recipe-app/src/layout/NavigationBottom.vue
@@ -3,7 +3,7 @@
   <div class="footer__content">
     <router-link :to="{ name: '' }"> <i class="icofont-refrigerator"></i></router-link> 
     <router-link :to="{ name : ''}"><i class="icofont-ui-calendar"></i></router-link>
-    <router-link :to="{ name: 'Feed' }"> <i class="icofont-ui-home"></i> </router-link> 
+    <router-link :to="{ name: 'Feed' }"> <span class="material-icons-round">home</span> </router-link> 
     <router-link :to="{name : ''}"> <i class="icofont-list"></i></router-link>
     <router-link :to="{ name: 'Search' }">  <i class="icofont-search-1"></i></router-link> 
   </div>
