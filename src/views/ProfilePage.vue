57bd967b32210330a9a28dff0023e085d8213c2c
Malene Ulm <msul36749@edu.ucl.dk>
2022-03-30 13:34:50 +0200

    log out function on profile page

--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -3,6 +3,12 @@
     <h1>Profile Page</h1>
     <p>{{ user.username }}</p>
     <p>{{ user.email }}</p>
+
+    <span @click="logout">
+                <router-link to="/" v-if="user" class="logout"
+                  >Logud</router-link
+                >
+              </span>
   </div>
 </template>
 
@@ -10,7 +16,17 @@
 export default {
   data() {
     return {
-      user: JSON.parse(window.localStorage.getItem("userData"))
+       user: {}
+    }
+  },
+  mounted() {
+    this.user = JSON.parse(window.localStorage.getItem("userData"));
+  },
+  methods: {
+    logout() {
+      window.localStorage.removeItem("jwt");
+      window.localStorage.removeItem("userData");
+      this.$router.go("/feed");
     }
   }
 }
