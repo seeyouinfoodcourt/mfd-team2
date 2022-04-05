3de7048df8ff3db19a81ed90ade0ad51432e7128
Malene <contact@maleneulm.dk>
2022-04-05 14:22:21 +0200

    Component: RecipeIngredient with placeholder text

--- recipe-app/src/components/recipe/RecipeIngredient.vue
+++ recipe-app/src/components/recipe/RecipeIngredient.vue
@@ -1,11 +1,16 @@
 <template>
-<div>{{ this.id }}</div>
-  
+<div class="ingredient__card">
+    <div class="ingredient__info">
+        <img src="" alt="" class="ingredient__img">img
+        <div class="ingredient__name">name</div>
+    </div>
+    <div class="ingredient__amount">amount</div>
+</div>
 </template>
 
 <script>
 export default {
-   
+    props: ['']
 
 }
 </script>
