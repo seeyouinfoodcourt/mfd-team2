f4506695f3dd226d62d22048a8e4f8ec382f96ad
Mikkel Desktop <msjo36725@edu.ucl.dk>
2022-04-06 17:51:52 +0200

    overlay with recipe data components

--- recipe-app/src/components/recipe/RecipeCard.vue
+++ recipe-app/src/components/recipe/RecipeCard.vue
@@ -1,36 +1,63 @@
 <template>
     
-        <div class="recipe-card" :class="slideWidth">
-        <img :src="cardImg" :alt="recipe.attributes.Title" class="recipe-img">
-        <div class="recipe-title">
+    <div class="card" :class="slideWidth">
+        <div class="card__header">
+            <img :src="cardImg" :alt="recipe.attributes.Title" class="card__img">
+            <div class="card__overlay">
+                <div class="topleft">
+                    <RecipeStats />
+                </div>
+                <div class="topright">
+                    <RecipeCardSocialButtons />
+                </div>
+                <div class="recipe-attributes">
+                    <div class="recipe-attributes__left">
+                        <p class="recipe-attributes__left__difficulty">{{ recipe.attributes.difficulty.data.attributes.Name }} <span class="emoji"></span> </p>
+                        <p class="recipe-attributes__left__ingredients">{{ numberOfIngredients }} ingredients</p>
+                    </div>
+                    <div class="recipe-attributes__right">
+                        <p class="recipe-attributes__rightcook-time">&#128336; {{ recipe.attributes.CookTime }}min</p>
+                    </div>
+                </div>                
+            </div>
+        </div>        
+        <div class="card__title">
             <h4 class="card-headline">{{ recipe.attributes.Title }}</h4>
-            <div class="card-body">
+            <div class="card__body">
                 <RecipeAuthor :author="cardAuthor"/>          
-                <router-link :to="{ name: 'RecipeDetails', params: { id: recipe.id} }">
-                <button class="button button--green" v-if="slideWidth === 'medium' || slideWidth === 'large'">Cook <i class="icofont-arrow-right"></i></button>
-            </router-link>
+                <router-link :to="{ name: 'RecipeDetails', params: { id: recipe.id} }" v-if="slideWidth === 'medium' || slideWidth === 'large'">
+                    <button class="button button--green">Cook <i class="icofont-arrow-right"></i></button>
+                </router-link>
             </div>
-            
-            </div>
+            <div class="card__gallery" v-if="slideWidth === 'full' ">
+                
+            </div>            
         </div>
-    
+    </div>
+  
  
 </template>
 
 <script>
 import RecipeAuthor from './RecipeAuthor.vue'
+import RecipeStats from './RecipeStats.vue'
+import RecipeCardSocialButtons from './RecipeCardSocialButtons.vue'
 
 export default {
     props: [ 'recipe', 'cardSize', 'slideWidth' ],
-    components: { RecipeAuthor },
+    components: { RecipeAuthor, RecipeStats, RecipeCardSocialButtons },
     data(){
         return{     
             cardAuthor: this.recipe.attributes.users_permissions_user.data,
-            cardImg: 'Test'
+            cardImg: 'Test',
+            numberOfIngredients: 14,
+            emoji: '&#128336;'
         }
     },
     mounted(){
+        document.querySelector('.emoji').innerHTML = '&#'+this.recipe.attributes.difficulty.data.attributes.ImageURL+';'
         this.cardImg = require('../../../public/img/food/'+this.recipe.attributes.ImageURL)
+        console.log(this.recipe.attributes)
         // console.log(this.recipe.attributes.Title, this.recipe.attributes.users_permissions_user.data.attributes.username)
         // console.log(this.recipe.attributes.Title, this.recipe.attributes.ImageURL)
         // console.log(this.recipe.id, this.cardImg)
@@ -39,10 +66,55 @@ export default {
 }
 </script> 
 
-<style scoped>
+<style lang="scss">
 
 
-.recipe-card {
+    .recipe-attributes{
+        flex:100%;
+        align-self:flex-end;
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-end;        
+        p{
+            margin:0;
+        }
+    }
+    
+    
+
+    .topleft {
+        flex: 50%; 
+    }
+
+    .bottomleft {
+        flex: 50%;
+    }
+    
+    .bottomleft,  .bottomright{
+      align-self:flex-end;
+      
+    }
+
+
+.card__header {
+    position: relative;
+}
+
+.card__overlay{
+    padding:1em;
+    color:white;
+    display: flex;
+    flex-wrap: wrap;
+    background: linear-gradient(0deg, #181818 0%, rgba(24, 24, 24, 0) 30.04%);
+    position: absolute;
+    z-index: 100;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+}
+
+.card {
     background-color: #fff;
     width: 100%;
     border-radius:8px; 
@@ -50,20 +122,20 @@ export default {
 }
 
 
-.recipe-img{
+.card__img{
     max-width:100%;
     border-radius: 8px 8px 0 0;
 }
-.recipe-title{
+.card__title{
     padding: 8px;    
 }
-.recipe-title h4{
+.card__title h4{
     margin: 0;
     text-overflow: ellipsis;
     overflow:hidden;
 }
 
-.card-body{
+.card__body{
     display: flex;
     align-items: center;
     justify-content: space-between;
