82fd5b5302bce8c25c6ac21e8f09621f199295ab
henrietteabbednaes <henr8588@edu.ucl.dk>
2022-04-12 09:00:28 +0200

    updata ingridient function

--- recipe-app/src/views/CreateRecipe.vue
+++ recipe-app/src/views/CreateRecipe.vue
@@ -3,10 +3,12 @@
   <div v-if="error">
     {{ error }}
   </div>
-  <form class="form__card" id="form" v-on:submit="handleSubmit" v-else>
-    <h1 style="text-align:center" class="headline">Create your delicious recipe</h1>
+
+  <form class="form__card" id="form" @submit="handleSubmit" v-else>
+    <h1 style="text-align:center" class="headline">Create your delicious recipe </h1>
     <label for="Title" class="form__label">Recipe title</label>
-    <input class="form__input" id="Title" v-model="modifiedData.Title" type="text" name="Title">
+    <input class="form__input" id="title" v-model="title" type="text" name="title">
+
 <br>
 <br>
 
@@ -17,20 +19,23 @@
 <br>
 
     <label for="Description" class="form__label">Description</label>
-    <textarea class="form__input" name="Description" id="Description" v-model="modifiedData.Description" cols="30" rows="5"></textarea> 
+    <textarea class="form__input" name="description" id="description" v-model="description" cols="30" rows="5"></textarea> 
 
 <br>
 <br>
+
+
     <label class="form__label">Cooking time</label>
-    <div class="flexContainer">
+ <div class="flexContainer">
+      
       <div class="flexItem">
       <label class="form__label form__label--time" for="timeHouers">Hours</label>
-      <input class="form__input" id="timeHouers" v-model="modifiedData.timeHouers" type="number" name="timeHouers">
+      <input class="form__input" id="timeHouers" v-model="timeHouers" type="number" name="timeHouers">
       </div>
 
       <div class="flexItem">
       <label class="form__label form__label--time" for="timeMinutes">Minutes</label>
-      <input class="form__input" id="timeMinutes" v-model="modifiedData.timeMinutes" type="number" name="timeMinutes">
+      <input class="form__input" id="timeMinutes" v-model="timeMinutes" type="number" name="timeMinutes">
       </div>
     </div>
 
@@ -38,9 +43,9 @@
 
     <label class="form__label">Select meal types</label>
     <div class="flexContainer">
-      <div class="flexItem" v-for="mealType in allMealTypes" :key="mealType.id">
-        <input class="from__input" type="checkbox" :value="mealType.id" v-model="modifiedData.mealType" name="allMealTypes" :id="mealType.id"/>
-        <label>{{ mealType.attributes.Name }}</label>
+      <div class="flexItem" v-for="meal in allMealTypes" :key="meal.id">
+        <input class="from__input" type="checkbox" :value="meal.id" v-model="mealType" name="allMealTypes" :id="meal.id"/>
+        <label>{{ meal.attributes.Name }}</label>
       </div>
     </div>
 
@@ -48,28 +53,30 @@
 
     <label class="form__label">Choose difficulty</label>
     <div class="flexContainer">
-      <div class="flexItem" v-for="difficulty in allDifficulties" :key="difficulty.id">
-        <input type="checkbox" :value="difficulty.id" v-model="modifiedData.difficulty" name="allDifficulties" :id="difficulty.id">
-       <label>{{ difficulty.attributes.Name }}</label>
+      <div class="flexItem" v-for="diff in allDifficulties" :key="diff.id">
+        <input type="checkbox" :value="diff.id" v-model="difficulty" name="allDifficulties" :id="diff.id">
+       <label>{{ diff.attributes.Name }}</label>
     </div>
     </div>
 
 <br>
     <label class="form__label">Select equipment</label>
      <div class="flexContainer">
-      <div class="flexItem" v-for="equipment in allEquipments" :key="equipment.id">
-        <input type="checkbox" :value="equipment.id" v-model="modifiedData.equipment" name="allEquipments" :id="equipment.id">
-       <label>{{ equipment.attributes.Name }}</label>
+      <div class="flexItem" v-for="equip in allEquipments" :key="equip.id">
+        <input type="checkbox" :value="equip.id" v-model="equipment" name="allEquipments" :id="equip.id">
+       <label>{{ equip.attributes.Name }}</label>
       </div>
     </div>
 
 <br>
+
 <!--INGRIDENSLISTE MED DROPDOWN-->
-   <label class="form__label">Select ingredients</label>
+
+ <label class="form__label">Select ingredients</label>
   <FormAddIngredient/>
 
   <br>  
-  <button class="button button--green" type="button">Create recipe</button>
+  <button class="button button--green">Create recipe</button>
   </form>
 </div>
 
@@ -80,8 +87,9 @@ import axios from 'axios';
 import FormAddIngredient from '../components/form/FormAddIngredient.vue'; 
 /*import FormAddSteps from '../components/form/FormAddSteps.vue'; */
 
+
 export default {
-  name: 'CreateRecipe',
+  name: 'CreateRecipeView',
   components: { 
     FormAddIngredient,
     /*FormAddSteps,*/
@@ -91,16 +99,15 @@ export default {
       allDifficulties: [],
       allMealTypes: [],
       allEquipments: [],  
-      modifiedData: {
-        Title: '',
-        Description: '',
-        timeMinutes: '',
-        timeHouers: '',
-        mealType: [],
-        difficulty: [],
-        equipment: [],
-        ingredient:[],
-      },
+
+      title: '',
+      description: '',
+      timeMinutes: '',
+      timeHouers: '',
+      mealType: [],
+      difficulty: [],
+      equipment: [],
+     
       error: null
     }; 
   },
@@ -111,29 +118,52 @@ async mounted() {
       this.allMealTypes = response.data.data;
       const response2 = await axios.get(`${process.env.VUE_APP_STRAPI}api/difficulties`)
       this.allDifficulties = response2.data.data; 
-      const response3 = await axios.get(`${process.env.VUE_APP_STRAPI}api/equipments`)
+
+      //const response3 = await axios.get(`${process.env.VUE_APP_STRAPI}api/equipments`)
+      const response3 = await axios.get('http://localhost:1337/api/equipments')
       this.allEquipments = response3.data.data; 
-      //console.log(response3)
+      //console.log(response3.data)
 
     } catch (error) {
       this.error = error;
     }
   },
 
-    handleSubmit: async function(e) {
+  methods:{
+  handleSubmit: async function(e) {
       e.preventDefault();
         try {
-        const response = await axios.post(`${process.env.VUE_APP_STRAPI}recipes`, this.modifiedData); 
-        console.log(response); 
+        const response = await this.axios.post('http://localhost:1337/api/recipes',
+        {
+        data:{
+            Title: this.title,
+            Description: this.description,
+            CookTime: this.timeMinutes,
+            meal_type: this.mealType, 
+            difficulty: this.difficulty,
+            equipment:this.equipment,
+            }
+        }); 
+        let recipeId = this.title.id; 
+        /*
+        const response2 = await this.axios.post('http://localhost:1337/api/recipe-ingredients',
+        {
+          data:{
+            recipe: 
+            Amount: 10, 
+            unit: 1,
+            ingridient: 2, 
+          }
+        });
+        console.log(response, response2); */
+  
       } catch(error) {
         this.error = error;
-
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
         
       }
-    }
+    },
+
+ }
  }; 
 
 </script>
