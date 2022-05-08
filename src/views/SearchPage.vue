f035bfa81d55bcdf77daece53cf45da24248ed7f
henrietteabbednaes <henr8588@edu.ucl.dk>
2022-05-08 15:16:51 +0200

    boilerplate on searchPage

--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -1,6 +1,8 @@
 <template>
-  <!-- <h1>Search Page</h1> -->
-  <SearchBar/>
+  <SearchBar />  <!--@input="onSearch"-->
+    <span v-for="recipe in filteredList" :key="recipe.id">
+      <p>{{ recipe.attributes.Title}}</p>
+    </span>
 </template>
 
 <script>
@@ -8,7 +10,31 @@ import SearchBar from '../components/search/SearchBar.vue'
 
 export default {
   components: {
-    SearchBar,
+    SearchBar,    
+  },
+  data(){
+    return{
+      recipes: [],
+      search:'',
+    }
+  },
+  mounted() {
+      fetch(`${process.env.VUE_APP_STRAPI}api/recipes?populate=*`).then(response => response.json()).then(data => this.recipes = data.data);
+    },
+
+  /*
+  methods: {
+      onSearch(query) {
+        console.log('search', query);
+      }
+  },*/
+
+  computed: {
+    filteredList() {
+      return this.recipes.filter(recipe => {
+        return recipe.attributes.Title.toLowerCase().match(this.search.toLowerCase())
+      })
+    }
   }
 }
 </script>
