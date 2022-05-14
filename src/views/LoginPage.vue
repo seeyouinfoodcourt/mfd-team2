359c160e923c109751dab389c5f91492c16b6786
Malene Ulm <msul36749@edu.ucl.dk>
2022-05-14 19:20:46 +0200

    LoginPage form validation

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -4,17 +4,25 @@
       <div class="">
         <div class="">
           <form class="form__card" @submit="login">
-            <div class="">
+            <!-- Email -->
+            <div class="" :class="{ error: v$.form.email.$errors.length }">
               <label class="form__label">Email</label><br>
-              <input class="form__input" type="email" v-model="email" />
+              <input class="form__input" type="email" v-model="v$.form.email.$model" />
+              <div v-for="(error, index) of v$.form.email.$errors" :key="index">
+                  <p class="form__error"> {{ error.$message }} </p>
+              </div>
             </div><br>
-            <div class="">
+            <!-- Password -->
+            <div class="" :class="{ error: v$.form.password.$errors.length }">
               <label class="form__label">Password</label><br>
-              <input class="form__input" type="password" v-model="password" />
+              <input class="form__input" type="password" v-model="v$.form.password.$model" />
+              <div v-for="(error, index) of v$.form.password.$errors" :key="index">
+                  <p class="form__error"> {{ error.$message }} </p>
+              </div>
             </div>
             <p v-show="error" class="form__error">{{ errorMsg }}</p>
             <br>
-            <button class="button button--green button--wide" type="submit" :disabled="password.length < 3">Log in</button>
+            <button class="button button--green button--wide" type="submit" :disabled="v$.form.$invalid">Log in</button>
             <p class="form__link"><br>
               <router-link to="/forgotpassword">Forgot password?</router-link>
             </p><br>
@@ -27,18 +35,43 @@
     </div>
   </div>
 </template>
-    <script>
+<script>
+import useVuelidate from '@vuelidate/core'
+import { required, email, minLength } from '@vuelidate/validators'
+
 export default {
+  setup() {
+    return { v$: useVuelidate() }
+  },
+
   name: "LoginPage",
 
   data() {
     return {
-      email: "",
-      password: "",
-      error: false,
-      errorMsg: "The email or password was incorrect",
-    };
+      form: {
+        email: '',
+        password: '',
+      },
+      errorMsg: "Email or password was incorrect. Try again.",
+      error: false
+    }
   },
+
+validations() {
+  return {
+    form: {
+      email: {
+        required, email
+      },
+      password: {
+        required,
+        min: minLength(7)
+      },
+    },
+  }
+
+},
+
   methods: {
     async login(e) {
       e.preventDefault();
@@ -46,8 +79,8 @@ export default {
         const res = await this.axios.post(
           `${process.env.VUE_APP_STRAPI}api/auth/local`,
           {
-            identifier: this.email,
-            password: this.password,
+            identifier: this.form.email,
+            password: this.form.password,
           }
         );
 
@@ -56,9 +89,8 @@ export default {
         window.localStorage.setItem("userData", JSON.stringify(user));
         this.$router.push("/");
         
-      } catch (error) {
+      } catch (e) {
         this.error = true;
-        this.password = "";
       }
     },
   },
