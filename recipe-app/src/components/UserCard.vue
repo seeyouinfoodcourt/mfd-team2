d99f1dc62d2c9092c3079019e682aa8faf7cf897
Mikkel <msjo36725@edu.ucl.dk>
2022-05-23 15:58:50 +0200

    popular chefs

---
+++ recipe-app/src/components/UserCard.vue
@@ -0,0 +1,36 @@
+<template>
+    <router-link class="link" :to="{ name: 'userPage', params: {id: 1} }">
+        <div class="user-card">
+            <img class="user-card__img" :src="profileImgUrl" :alt="profileImgAlt">
+            <p class="user-card__name">{{ user.username }}</p>
+        </div>
+    </router-link>
+
+</template>
+
+<script>
+export default {
+    props: [ 'user' ],
+    data() {
+        return {
+            profileImgUrl: '',
+            profileImgAlt: '',
+            users: ''
+        }
+    },
+    mounted () {
+        try {
+            this.profileImgUrl = require("../../public/img/profile/person" +
+            this.user.id +
+            ".jpg");
+            } catch (ex) {
+                this.profileImgUrl = "/mfd-team2/img/profile/profile-placeholder.png";
+            }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
